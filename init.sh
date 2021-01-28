1157383ca05d4b79db78a0d9f20bb19e1fdd80f0
xushaodong <xushaodong@bytedance.com>
2021-01-28 16:04:20 +0800

    update SpaceVim.d

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask iterm2
+brew install --cask iterm2 rectangle
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chmod 755 /usr/local/share/zsh\nchmod 755 /usr/local/share/zsh/site-functions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -15,7 +15,7 @@ npm i -g javascript-typescript-langserver typescript-language-server
 pip3 install python-language-server
 
 curl -sLf https://spacevim.org/cn/install.sh | bash
-brew install neovim
+brew install neovim ripgrep 
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh\n
 curl https://github.com/rust-analyzer/rust-analyzer/releases/download/nightly/rust-analyzer-mac -o ~/.cargo/bin/rust-analyzer
