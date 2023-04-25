fed4ff5df1a63d16e521f07c4a1e523950bce737
Shull <xusd320@gmail.com>
2023-04-25 19:58:33 +0800

    update telescope

--- lua/telescope_setup.lua
+++ lua/telescope_setup.lua
@@ -10,6 +10,18 @@ for b, _ in pairs(lvim.builtin.telescope.pickers) do
   lvim.builtin.telescope.pickers[b].theme = nil
   lvim.builtin.telescope.pickers[b].show_line = false
 end
+lvim.builtin.telescope.pickers.lsp_definitions = {
+  show_line = false
+}
+lvim.builtin.telescope.pickers.lsp_references = {
+  show_line = false
+}
+lvim.builtin.telescope.pickers.lsp_implementations = {
+  show_line = false
+}
+lvim.builtin.telescope.pickers.lsp_type_definitions = {
+  show_line = false
+}
 lvim.builtin.telescope.pickers.diagnostics = {
   line_width = 0
 }
