03d8f361431aebb5f8e01cad0e6e2352ed06d983
Shull <xusd320@gmail.com>
2023-04-28 11:49:45 +0800

    update nvimtree git timeout

--- lua/filetree.lua
+++ lua/filetree.lua
@@ -3,4 +3,5 @@ lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = false
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.nvimtree.setup.git.ignore = true
+lvim.builtin.nvimtree.setup.git.timeout = 400
 lvim.builtin.nvimtree.setup.filters.custom = {}
