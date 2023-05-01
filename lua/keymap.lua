6506494c95207d662199c4208e3a295cabfe244e
Shull <xusd320@gmail.com>
2023-05-01 11:33:26 +0800

    update keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,10 +1,9 @@
 lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.lsp.buffer_mappings.normal_mode["ga"] = { vim.lsp.buf.declaration, "Goto declaration" }
-lvim.lsp.buffer_mappings.normal_mode["gd"] = { "<cmd>Telescope lsp_definitions<cr>", "Goto definition" }
+lvim.lsp.buffer_mappings.normal_mode["gd"] = { "<cmd>Telescope lsp_definitions<cr>", "Goto definitions" }
+lvim.lsp.buffer_mappings.normal_mode["gD"] = { vim.lsp.buf.declaration, "Goto declarations" }
 lvim.lsp.buffer_mappings.normal_mode["gt"] = { "<cmd>Telescope lsp_type_definitions<cr>", "Goto type definitions" }
 lvim.lsp.buffer_mappings.normal_mode["gi"] = { "<cmd>Telescope lsp_implementations<cr>", "Goto implementations" }
 lvim.lsp.buffer_mappings.normal_mode["gr"] = { "<cmd>Telescope lsp_references<cr>", "Goto references" }
@@ -24,9 +23,16 @@ lvim.builtin.which_key.setup.plugins = {
   },
   spelling = { enabled = true, suggestions = 20 },
 }
+
+lvim.builtin.which_key.mappings["c"] = nil
+
 lvim.builtin.which_key.mappings["b"]["a"] = {
   "<cmd>:enew<cr>",
   "New Buffer",
+}
+lvim.builtin.which_key.mappings["b"]["c"] = {
+  "<cmd>BufferKill<cr>",
+  "Kill Buffer",
 }
 lvim.builtin.which_key.mappings["b"]["y"] = {
   "<cmd>:%y<cr>",
@@ -36,17 +42,7 @@ lvim.builtin.which_key.mappings["b"]["d"] = {
   "<cmd>:%d<cr>",
   "Clear Buffer",
 }
-lvim.builtin.which_key.mappings["l"]["c"] = {
-  vim.diagnostic.open_float,
-  "Current Diagnostic",
 
-}
-lvim.builtin.which_key.mappings["r"] = {
-  name = "Spectre",
-  o = {
-    "<cmd>Spectre<cr>", "Open Spectre"
-  }
-}
 lvim.builtin.which_key.mappings["t"] = {
   name = "Telescope",
   t = {
@@ -54,14 +50,26 @@ lvim.builtin.which_key.mappings["t"] = {
   },
   r = {
     "<cmd>Telescope resume<cr>", "Resume"
+  },
+  p = {
+    "<cmd>Telescope projects<cr>", "Projects"
+  }
+}
+
+lvim.builtin.which_key.mappings["s"]["R"] = {
+  name = "Spectre",
+  o = {
+    "<cmd>Spectre<cr>", "Open Spectre"
   }
 }
 lvim.builtin.which_key.mappings["s"]["d"] = {
   "<cmd>Telescope grep_string<cr>", "Search word under cursor"
 }
+
 lvim.builtin.which_key.mappings["T"] = {
   "<cmd>ToggleTerm<cr>", "Toggle Term"
 }
+
 lvim.builtin.which_key.mappings["h"] = {
   "<cmd>DogeGenerate<cr>", "DogeGenerate"
 }
