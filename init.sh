342debf0561f72a507be2faff73028767f41b8a7
Shull <xusd320@gmail.com>
2023-05-01 17:04:37 +0800

    update folder

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ ln -s xsh/zshrc ~/.zshrc
 ln -s xsh/p10k.zsh ~/.p10k.zsh
 
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
-ln -s ~/xsh ~/.config/lvim
+ln -s ~/xsh/nvim ~/.config/lvim
 
 mkdir -p ~/.config/tmux
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
