9982a4690fd2f0d9e1d0850842dd9e6b70b42a85
Shull <xusd320@gmail.com>
2023-05-23 10:00:43 +0800

    update lualine

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -37,8 +37,8 @@ lvim.builtin.terminal.active = true
 
 lvim.builtin.lualine.sections = {
   lualine_a = { 'mode' },
-  lualine_b = { 'branch', 'diff', 'diagnostics' },
-  lualine_c = { { 'filename', path = 1 } },
+  lualine_b = { 'branch', 'diff', { 'filename', path = 1 } },
+  lualine_c = { 'diagnostics' },
   lualine_x = { 'encoding', 'fileformat', 'filetype' },
   lualine_y = { 'progress' },
   lualine_z = { 'location' }
