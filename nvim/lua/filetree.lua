38b92f7da499b8c3a7cd9eee8df5c56f93c54855
Shull <xusd320@gmail.com>
2023-06-15 21:41:00 +0800

    fix nvim-tree

--- nvim/lua/filetree.lua
+++ nvim/lua/filetree.lua
@@ -5,3 +5,8 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.nvimtree.setup.git.ignore = true
 lvim.builtin.nvimtree.setup.git.timeout = 400
 lvim.builtin.nvimtree.setup.filters.custom = {}
+lvim.builtin.nvimtree.setup.filesystem_watchers = {
+  ignore_dirs = {
+    "^.git$",
+  },
+}
