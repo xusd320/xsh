bb917fde11793a488cc99bc0c87746435a028bcc
Shull <xusd320@gmail.com>
2023-06-15 21:51:03 +0800

    update nvim-tree

--- nvim/lua/filetree.lua
+++ nvim/lua/filetree.lua
@@ -1,5 +1,3 @@
-lvim.builtin.nvimtree.setup.update_cwd = false
-lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = false
 lvim.builtin.nvimtree.setup.view.side = 'right'
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.nvimtree.setup.git.ignore = true
