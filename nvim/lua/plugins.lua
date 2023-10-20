f2e4eb42d370afc7bdf361310af61ba9c473dbfa
Shull <xusd320@gmail.com>
2023-10-20 20:11:46 +0800

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -102,7 +102,7 @@ lvim.plugins = {
       require('ufo').setup({
         fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
           local newVirtText = {}
-          local suffix = ('  %d '):format(endLnum - lnum)
+          local suffix = (' ⬿ %d '):format(endLnum - lnum)
           local sufWidth = vim.fn.strdisplaywidth(suffix)
           local targetWidth = width - sufWidth
           local curWidth = 0
