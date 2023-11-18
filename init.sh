0ad37252c02907ba9241332ccdd8f3683d067c92
xusd320 <xusd320@U-47YM91X2-0100.local>
2023-11-18 16:26:13 +0800

    update nvim

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install wget llvm bat jq tmux neovim ripgrep cmake alacritty
-brew tap homebrew/cask-fonts 
+brew tap homebrew/cask-fonts
 brew install --cask clashx rectangle font-hack-nerd-font visual-studio-code rectangle maccy
 
-git clone git@github.com:xusd320/xsh.git 
+git clone git@github.com:xusd320/xsh.git
 git clone git@github.com:dracula/alacritty.git ~/.config/alacritty
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -12,7 +12,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-ln -s xsh/zshrc ~/.zshrc 
+ln -s xsh/zshrc ~/.zshrc
 ln -s xsh/p10k.zsh ~/.p10k.zsh
 
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
@@ -20,24 +20,23 @@ ln -s ~/xsh/nvim ~/.config/lvim
 
 sudo chown -R $(whoami) /usr/local/bin
 
-git clone https://github.com/vovkasm/input-source-switcher.git                                                                                                                                                 ✔ 
+git clone https://github.com/vovkasm/input-source-switcher.git  ✔ 
 cd input-source-switcher
 mkdir build && cd build
 cmake ..
 make
 make install
 
-
 mkdir -p ~/.config/tmux
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 ln -s ~/xsh/tmux.conf ~/.config/tmux/tmux.conf
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-nvm install lts/fermium 
+nvm install lts/fermium
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sudo xcode-select -switch /Library/Developer/CommandLineTools 
+sudo xcode-select -switch /Library/Developer/CommandLineTools
 
 brew install tabbyml/tabby/tabby
 tabby serve --device metal --model TabbyML/StarCoder-1B
