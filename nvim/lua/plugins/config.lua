c2f1eefa9b067f5c3349ce6f01b071afa41409b0
xusd320 <xusd320@gmail.com>
2023-11-19 18:50:50 +0800

    update nvim

--- nvim/lua/plugins/config.lua
+++ nvim/lua/plugins/config.lua
@@ -8,6 +8,22 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      routes = {
+        {
+          view = "cmdline",
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "%d fewer lines" },
+              { find = "%d more lines" },
+            },
+          },
+          opts = { skip = true },
+        },
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
