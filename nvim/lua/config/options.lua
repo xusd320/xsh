15430b5b5b417cb652e6350ed78f953a62d53514
Shull <xusd320@gmail.com>
2023-11-23 23:35:04 +0800

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,5 +5,5 @@ vim.opt.cmdheight = 0
 vim.opt.backup = true
 vim.opt.backupdir = vim.fn.expand("~/.local/state/nvim/backup")
 vim.opt.swapfile = false
-vim.opt.undofile = true
+vim.opt.undofile = false
 vim.g.root_spec = { "cwd" }
