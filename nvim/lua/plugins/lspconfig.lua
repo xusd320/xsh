247b729dc10ef37fb644e7d764bd47c1a059c2ba
Shull <xusd320@gmail.com>
2023-11-23 19:00:03 +0800

    Update lspconfig.lua

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,23 @@
+local get_git_root_dir = function(fname)
+  local util = require("lspconfig.util")
+  return util.root_pattern(".git")(fname)
+end
+
+local get_typescript_root_dir = function(fname)
+  local util = require("lspconfig.util")
+  return get_git_root_dir(fname) or util.root_pattern("package.json", "tsconfig.json")(fname)
+end
+
 return {
-  {
-    "neovim/nvim-lspconfig",
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      eslint = {
+        root_dir = get_typescript_root_dir,
+      },
+      tsserver = {
+        root_dir = get_typescript_root_dir,
+      },
+    },
   },
 }
