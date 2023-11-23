7e1ac2cd524927ad6eb913b14c399d544f87982f
Shull <xusd320@gmail.com>
2023-11-23 14:14:56 +0800

    update nvim

---
+++ nvim/lua/plugins/project.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = false,
+      detection_methods = { "lsp", "pattern" },
+      patterns = { ".git" },
+    },
+  },
+}
