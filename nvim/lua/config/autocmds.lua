3e21bf514907133c58eae10faf705ac4dd460bcb
Shull <xusd320@gmail.com>
2023-12-01 15:56:34 +0800

    update tmux

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,14 +3,3 @@ vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
-
-vim.api.nvim_create_autocmd({ "DirChanged" }, {
-  group = augroup("startsyncdirtotmux"),
-  pattern = "global",
-  command = 'call system("bash ~/xsh/tmux_vim.sh attach")',
-})
-
-vim.api.nvim_create_autocmd({ "ExitPre" }, {
-  group = augroup("stopsyncdirtotmux"),
-  command = 'call system("bash ~/xsh/tmux_vim.sh unattach")',
-})
