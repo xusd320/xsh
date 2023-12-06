17575056c9291065d0a312fea289c399b32da963
Shull <xusd320@gmail.com>
2023-12-06 16:32:22 +0800

    update nvim-ufo

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -44,7 +44,6 @@ return {
           return { "treesitter" }
         end,
         open_fold_hl_timeout = 400,
-        close_fold_kinds = { "imports", "comment" },
         preview = {
           win_config = { border = { "", "─", "", "", "", "─", "", "" }, winblend = 0 },
           mappings = {
