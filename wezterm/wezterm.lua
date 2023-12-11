bd0bea4d29427c46d7db9aae1184bd61ca2e7d30
Shull <xusd320@gmail.com>
2023-12-11 21:58:53 +0800

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,23 +3,31 @@ require("format-tab-title")
 
 local wezterm = require("wezterm")
 
-local wezconf = wezterm.config_builder()
+local config = wezterm.config_builder()
 
-wezconf.font = wezterm.font("Hack Nerd Font")
+config.font = wezterm.font("Hack Nerd Font")
 
-wezconf.font_size = 14.0
+config.font_size = 14.0
 
-wezconf.color_scheme = "Catppuccin Mocha"
+config.freetype_load_target = "Light"
 
-wezconf.window_padding = {
+config.freetype_render_target = "HorizontalLcd"
+
+config.cell_width = 0.9
+
+config.front_end = "OpenGL"
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
 
-wezconf.window_decorations = "RESIZE"
+config.window_decorations = "RESIZE"
 
-wezconf.native_macos_fullscreen_mode = true
+config.native_macos_fullscreen_mode = true
 
-return wezconf
+return config
