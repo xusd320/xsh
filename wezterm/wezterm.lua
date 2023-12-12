3ef0ae947a490b7ca02cec337dd3c7b96eb69cd8
Shull <xusd320@gmail.com>
2023-12-12 19:16:52 +0800

    fix wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font_with_fallback("Hack Nerd Font")
+config.font = wezterm.font_with_fallback({ "Hack Nerd Font" })
 
 config.font_size = 14.0
 
