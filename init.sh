40d92671dfd220666e845ca092332a89fc71dcab
Shull <xusd320@gmail.com>
2023-12-13 23:01:36 +0800

    use starship

--- init.sh
+++ init.sh
@@ -1,19 +1,15 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew tap homebrew/cask-fonts daipeihust/tap
-brew install wget llvm bat jq neovim ripgrep fd cmake wezterm git-delta im-select fnm
+brew install wget llvm bat jq neovim ripgrep fd cmake wezterm starship antigen git-delta exa im-select fnm
 brew install --cask clashx rectangle font-hack-nerd-font visual-studio-code rectangle maccy
 
 git clone git@github.com:xusd320/xsh.git
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-ln -s ~/xsh/gitconfig ~/.gitconfig
 ln -s ~/xsh/wezterm ~/.config/wezterm
-ln -s xsh/zshrc ~/.zshrc
+ln -s ~/xsh/zshrc ~/.zshrc
+ln -s ~/xsh/starship.toml ~/.config/starship.toml
+ln -s ~/xsh/gitconfig ~/.gitconfig
 ln -s ~/xsh/lazygit ~/.config/lazygit
 
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
