8cf1dbdd409106874411aface350bb1492ac9008
Shull <xusd320@gmail.com>
2023-12-13 12:53:47 +0800

    update nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,13 @@
 vim.opt.cmdheight = 0
 vim.opt.relativenumber = false
+vim.opt.wrap = true 
+vim.opt.ignorecase = false
+vim.opt.smartcase = false
+vim.opt.showmode = true
 vim.opt.backup = true
 vim.opt.backupdir = vim.fn.expand("~/.local/state/nvim/backup")
 vim.opt.swapfile = false
 vim.opt.undofile = true
+
 vim.g.root_spec = { "cwd" }
 
