d0e5fd9e849775dd6029d2a3a2fdf16d6139f64d
Shull <xusd320@gmail.com>
2024-01-09 13:03:15 +0800

    nvim: update neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       window = {
         position = "right",
+        mappings = {
+          Y = "copy_selector",
+        }
       },
       reveal = true,
       filesystem = {
@@ -49,6 +52,39 @@ return {
           enabled = false,
         },
       },
+      commands = {
+        copy_selector = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local vals = {
+            ["BASENAME"] = modify(filename, ":r"),
+            ["EXTENSION"] = modify(filename, ":e"),
+            ["FILENAME"] = filename,
+            ["PATH (CWD)"] = modify(filepath, ":."),
+            ["PATH (HOME)"] = modify(filepath, ":~"),
+            ["PATH"] = filepath,
+            ["URI"] = vim.uri_from_fname(filepath),
+          }
+
+          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+          if vim.tbl_isempty(options) then
+            return
+          end
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = "Choose to copy to clipboard:",
+            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              vim.fn.setreg("+", result)
+            end
+          end)
+        end,
+      }
     },
   },
 }
