c4dfd3877eb68780cb88b01fcd51b08ff466aa46
Shull <xusd320@gmail.com>
2024-02-20 10:17:30 +0800

    nvim: update autopairs

--- nvim/lua/plugins/pairs.lua
+++ nvim/lua/plugins/pairs.lua
@@ -10,7 +10,9 @@ return {
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       local cmp = require("cmp")
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      require("nvim-autopairs").setup({})
+      require("nvim-autopairs").setup({  
+        enable_check_bracket_line = false
+      })
     end,
     keys = {
       {
