c764d468f92edb2131b9a840ae22753fb82671d6
xusd320 <xusd320@gmail.com>
2024-03-09 21:26:52 +0800

    nvim: update formatter

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,49 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_active_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettier" }
+  end
+  return { "biome" }
+end
+
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = biome_lsp_or_prettier,
+        ["javascriptreact"] = biome_lsp_or_prettier,
+        ["typescript"] = biome_lsp_or_prettier,
+        ["typescriptreact"] = biome_lsp_or_prettier,
+        ["json"] = biome_lsp_or_prettier,
+        ["jsonc"] = biome_lsp_or_prettier,
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
+      },
+    },
+  },
+}
