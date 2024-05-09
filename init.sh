0fabe759bb1b805fad8c767b904d5cbc7c67a016
xusd320 <xusd320@gmail.com>
2024-05-09 21:10:01 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 brew tap homebrew/cask-fonts 
 brew tap daipeihust/tap
 brew install wget llvm bat jq neovim ripgrep fd wezterm starship git-delta eza im-select fnm
-brew install --cask cmake clashx rectangle font-hack-nerd-font visual-studio-code rectangle maccy
+brew install --cask cmake rectangle font-hack-nerd-font visual-studio-code rectangle maccy
 
 git clone git@github.com:xusd320/xsh.git
 
