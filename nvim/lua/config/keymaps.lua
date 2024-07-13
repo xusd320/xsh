a5d9c753af23d9c63a8530fea1ceff3d13c46a97
xusd320 <xusd320@gmail.com>
2024-07-13 19:54:34 +0800

    nvim: update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,53 +1,15 @@
 local wk = require("which-key")
 
-wk.register({
-  ["<leader><Space>"] = {
-    name = "Telescope",
-    t = {
-      "<cmd>Telescope<cr>",
-      "Toggle",
-    },
-    r = {
-      "<cmd>Telescope resume<cr>",
-      "Resume",
-    },
-    p = {
-      "<cmd>Telescope projects<cr>",
-      "Projects",
-    },
-  },
-  ["<leader>fr"] = {
-    LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }),
-    "Recent (cwd)",
-  },
-  ["<leader>fR"] = {
-    "<cmd>FzfLua oldfiles<cr>",
-    "Recent",
-  },
-  ["gl"] = {
-    name = "Goto call hierarchy",
-    i = {
-      "<cmd>Telescope lsp_incoming_calls<cr>",
-      "Goto incoming calls",
-    },
-    o = {
-      "<cmd>Telescope lsp_outgoing_calls<cr>",
-      "Goto outgoing calls",
-    },
-  },
-})
-
-wk.register({
-  ["<leader>ba"] = {
-    "<cmd>:enew<cr>",
-    "New Buffer",
-  },
-  ["<leader>bx"] = {
-    "<cmd>:%d<cr>",
-    "Clear Buffer",
-  },
-  ["<leader>by"] = {
-    "<cmd>:%y<cr>",
-    "Copy Buffer",
-  },
+wk.add({
+  { "<leader><Space>", group = "Telescope" },
+  { "<leader><Space>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
+  { "<leader><Space>t", "<cmd>Telescope<cr>", desc = "Toggle" },
+  { "<leader>fR", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+  { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+  { "gl", group = "Goto call hierarchy" },
+  { "gli", "<cmd>Telescope lsp_incoming_calls<cr>", desc = "Goto incoming calls" },
+  { "glo", "<cmd>Telescope lsp_outgoing_calls<cr>", desc = "Goto outgoing calls" },
+  { "<leader>ba", "<cmd>:enew<cr>", desc = "New Buffer" },
+  { "<leader>bx", "<cmd>:%d<cr>", desc = "Clear Buffer" },
+  { "<leader>by", "<cmd>:%y<cr>", desc = "Copy Buffer" },
 })
