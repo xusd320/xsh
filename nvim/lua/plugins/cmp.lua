af9b97db900c5b189e591627fca17c046d0cedc1
xusd320 <xusd320@gmail.com>
2024-07-25 16:56:48 +0800

    nvim: update cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,9 +6,24 @@ return {
     },
     opts = function(_, opts)
       local cmp = require("cmp")
+      local compare = require("cmp.config.compare")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      })
+      vim.tbl_extend("force", opts.sorting, {
+        comparators = {
+          compare.kind,
+          compare.offset,
+          compare.exact,
+          compare.score,
+          compare.locality,
+          -- compare.recently_used,
+          -- compare.length,
+          -- compare.order,
+          -- compare.scopes,
+          -- compare.sort_text,
+        },
       })
       table.insert(opts.sources, {
         name = "spell",
