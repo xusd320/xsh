e9659099cfc2bf6af232ff2ebfa0bb542300ea57
xusd320 <xusd320@gmail.com>
2024-09-11 23:42:03 +0800

    nvim: update config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,11 +7,20 @@ return {
     opts = {
       highlight = {
         additional_vim_regex_highlighting = false,
-        disable = function(_, buf)
+        disable = function(_, bufnr)
           local max_filesize = 1000 * 1024
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
+          local max_line_count = 100 * 1000
+          local max_filesize_perline = 1024
+          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+          if ok and stats then
+            local line_count = vim.api.nvim_buf_line_count(bufnr)
+            if
+              stats.size > max_filesize
+              or line_count > max_line_count
+              or stats.size / line_count > max_filesize_perline
+            then
+              return true
+            end
           end
         end,
       },
