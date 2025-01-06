09b3ebbaddcf91a57eae779185e12de76f1bba8b
xusd320 <xusd320@gmail.com>
2025-01-07 00:48:05 +0800

    nvim: update blink config

--- nvim/lua/plugins/blinkcmp.lua
+++ nvim/lua/plugins/blinkcmp.lua
@@ -1,6 +1,21 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      {
+        "uga-rosa/cmp-dictionary",
+        dependencies = {
+          "saghen/blink.compat",
+        },
+        config = function()
+          require("cmp_dictionary").setup({
+            paths = { "/usr/share/dict/words" },
+            exact_length = 4,
+            max_number_items = 10,
+          })
+        end,
+      },
+    },
     opts = {
       completion = {
         list = {
@@ -14,6 +29,33 @@ return {
         ["<S-Tab>"] = { "select_prev", "fallback" },
       },
       signature = { enabled = true },
+      sources = {
+        default = {
+          "dictionary",
+        },
+        providers = {
+          dictionary = {
+            name = "dictionary",
+            module = "blink.compat.source",
+            opts = {
+              keyword_length = 4,
+            },
+          },
+        },
+      },
+      fuzzy = {
+        sorts = {
+          "score",
+          "kind",
+          function(a, b)
+            local sort = require("blink.cmp.fuzzy.sort")
+            if a.source_id == "dictionary" and b.source_id == "dictionary" then
+              return sort.label(a, b)
+            end
+          end,
+          "label",
+        },
+      },
     },
   },
 }
