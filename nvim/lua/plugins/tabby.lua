7fa1268b94acab7455738b09485bfe683278931a
xusd320 <xusd320@gmail.com>
2025-01-21 17:18:57 +0800

    nvim: use neogen, tabby and mini-surround

--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -2,9 +2,14 @@ return {
   {
     "TabbyML/vim-tabby",
     lazy = false,
-    config = function()
-      vim.g.tabby_keybinding_accept = "<C-Tab>"
-      vim.g.tabby_keybinding_trigger_or_dismiss = "<C-\\>"
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    init = function()
+      vim.g.tabby_agent_start_command = { "npx", "tabby-agent", "--stdio" }
+      vim.g.tabby_inline_completion_trigger = "auto"
+      vim.g.tabby_inline_completion_keybinding_accept = "<C-Tab>"
+      vim.g.tabby_inline_completion_keybinding_trigger_or_dismiss = "<C-\\>"
     end,
   },
 }
