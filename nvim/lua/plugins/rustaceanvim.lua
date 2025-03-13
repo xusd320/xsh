a0a21a1281585d54f602eeb11327be7728203ec4
xusd320 <xusd320@gmail.com>
2025-03-13 10:22:42 +0800

    nvim: update rustacean opts

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -3,13 +3,6 @@ return {
     "mrcjkb/rustaceanvim",
     opts = {
       server = {
-        cmd = function()
-          local mason_registry = require("mason-registry")
-          local ra_binary = mason_registry.is_installed("rust-analyzer")
-              and require("mason-core.path").bin_prefix("rust-analyzer")
-            or "rust-analyzer"
-          return { ra_binary }
-        end,
         default_settings = {
           ["rust-analyzer"] = {
             cachePriming = {
