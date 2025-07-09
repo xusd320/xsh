7c306de493bb1cd53e8bbc975645a1beb7a2630e
xusd320 <xusd320@gmail.com>
2025-07-09 10:48:06 +0800

    nvim: update tree config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,8 +13,6 @@ vim.opt.backupdir = vim.fn.expand("~/.local/state/nvim/backup")
 vim.opt.swapfile = false
 vim.opt.undofile = true
 
-vim.g.root_spec = { "cwd" }
-
 vim.g.snacks_animate = false
 
 vim.g.lazyvim_prettier_needs_config = true
