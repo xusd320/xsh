5ae580a939c2fc97986e95f85f710b1227b479fe
xusd320 <xusd320@gmail.com>
2025-07-31 01:28:52 +0800

    nvim: update codecompanion

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -10,6 +10,31 @@ return {
       build = "pipx upgrade vectorcode",
       dependencies = { "nvim-lua/plenary.nvim" },
     },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      ft = { "markdown", "codecompanion" },
+    },
+    {
+      "echasnovski/mini.diff",
+      config = function()
+        local diff = require("mini.diff")
+        diff.setup({
+          source = diff.gen_source.none(),
+        })
+      end,
+    },
+    {
+      "HakonHarnes/img-clip.nvim",
+      opts = {
+        filetypes = {
+          codecompanion = {
+            prompt_for_file_name = false,
+            template = "[Image]($FILE_PATH)",
+            use_absolute_path = true,
+          },
+        },
+      },
+    },
   },
   config = function()
     require("codecompanion").setup({
