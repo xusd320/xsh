ce7da441d47f99d50933b02c53eb07b563a92d66
xusd320 <xusd320@gmail.com>
2025-11-20 19:29:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-brew tap daipeihust/tap laishulu/homebrew
+brew tap daipeihust/tap 
+brew tap laishulu/homebrew
 brew install wget llvm lld bat jq neovim ripgrep fzf fd starship git-delta eza procs fnm macism ncdu cloc mcfly dua-cli im-select macism
 brew install zsh zsh-syntax-highlighting zsh-autosuggestions
 brew install --cask cmake font-hack-nerd-font visual-studio-code rectangle maccy switchhosts wezterm@nightly
