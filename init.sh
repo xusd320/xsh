66949382a7516ad5cf3cfff89ab498279dd40d1a
xusd320 <xusd320@gmail.com>
2025-12-11 12:28:22 +0800

    Add xcode-select installation command

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
