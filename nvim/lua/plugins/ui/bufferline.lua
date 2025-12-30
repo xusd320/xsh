91bc0399cebbbc3bbc6795acc0583b0cf1d0867c
xusd320 <xusd320@gmail.com>
2025-12-31 01:12:19 +0800

    feat: add bufferline keymaps for closing buffers

--- nvim/lua/plugins/ui/bufferline.lua
+++ nvim/lua/plugins/ui/bufferline.lua
@@ -4,8 +4,10 @@ return {
   keys = {
     { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
     { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
     { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
     { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<leader>bd", "<cmd>bd<cr>", desc = "Delete Buffer" },
     { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
     { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
